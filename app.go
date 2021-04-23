4f143a891a4b89f62424022a79f250bf8f57c10d
C9-LinkRs <linkrsfan@gmail.com>
2021-04-23 04:25:23 +0200

    implementing endpoints

--- app.go
+++ app.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -34,6 +36,53 @@ func (app *App) Run() {
 	log.Fatal(http.ListenAndServe(":2600", app.Router))
 }
 
-func (app *App) getTicket() {
+func (app *App) getTicket(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ticket Id")
+		return
+	}
 
+	t := ticket{ID: id}
+	if err := t.getTicket(app.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Ticket not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJson(w, http.StatusOK, t)
+}
+
+func (app *App) getAllTickets(w http.ResponseWriter, r *http.Request) {
+	count := 10
+	start, _ := strconv.Atoi(r.FormValue("start"))
+
+	if start < 0 {
+		start = 0
+	}
+
+	tickets, err := getAllTickets(app.DB, start, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJson(w, http.StatusOK, tickets)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJson(w, code, map[string]string{"error": message})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
